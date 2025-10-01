4562583bce634e6790e2a85b1aa2f5a1ac958304
Saad Ahmad <150076462+swahmad2005@users.noreply.github.com>
2025-10-01 03:15:25 +0000

    Fixes (5th attempt)

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -4,6 +4,6 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 def test_sample_run_anonymizer():
     result = sample_run_anonymizer("My name is Bond.", 11, 15)
     assert result.text == "My name is BIP."
-    assert result.items[0].end - result.items[0].start == 3
+    assert len(result.items) == 1
     assert result.items[0].start == 11
     assert result.items[0].end == 14
